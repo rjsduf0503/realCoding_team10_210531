82a84072182d7dacfc397ce9813e43cd5b990297
Byun sumi <selina2000@naver.com>
2021-05-20 23:16:34 +0900

    12. border

--- App.js
+++ App.js
@@ -55,6 +55,8 @@ const styles = StyleSheet.create({
   },
   buttonContainer : {
     alignItems: 'center',
-    width: 300,
+    width: '100%',
+    borderWidth: 2,
+    borderColor: 'purple',
   }
 });
