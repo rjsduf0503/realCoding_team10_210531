af18fce7cf1ab8137dd9c2bdb3976d9c9aae7030
Byun sumi <selina2000@naver.com>
2021-05-20 22:19:43 +0900

    07 add Component class

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
+class CounterButton extends React.Component {
   state = {
     counter :0,
   };
@@ -18,14 +18,21 @@ export default class App extends React.Component {
       <View style={styles.container}>
         <Text>{this.state.counter}</Text>
         <Button title={'click me!'} onPress={clickHandler}/>
-        <StatusBar style="auto" />
       </View>
     );
   };
   
 };
-
-
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <CounterButton />
+        <StatusBar style="auto" />
+      </View>
+    );  
+  }
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
