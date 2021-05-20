facfb7965165814841f2daf33d2c7d4c6e276038
Byun sumi <selina2000@naver.com>
2021-05-20 23:20:58 +0900

    13. flexbox, row

--- App.js
+++ App.js
@@ -2,45 +2,48 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 
-class CounterButton extends React.Component {
-  static defaultProps = {
-    // 초기값을 입력하지 않았을경우에 -1로 초기화
-    counter: -1,
-  };
-  constructor(props) {
-    super(props);
+// class CounterButton extends React.Component {
+//   static defaultProps = {
+//     // 초기값을 입력하지 않았을경우에 -1로 초기화
+//     counter: -1,
+//   };
+//   constructor(props) {
+//     super(props);
 
-    this.state = {
-      // 카운트 초기값 설정
-      counter: props.counter,
-    };
-  }
+//     this.state = {
+//       // 카운트 초기값 설정
+//       counter: props.counter,
+//     };
+//   }
 
-  render() {
-    // 클릭할때 마다 COUNT가 하나씩 증가하는 함수 
-    const  clickHandler = () => {
-      this.setState({
-        counter : this.state.counter+1,
-      });
-    }
-    return (
-      <View style={styles.buttonContainer}>
-        <Text>{this.state.counter}</Text>
-        <Button title={'click me!'} onPress={clickHandler}/>
-      </View>
-    );
-  };
+//   render() {
+//     // 클릭할때 마다 COUNT가 하나씩 증가하는 함수 
+//     const  clickHandler = () => {
+//       this.setState({
+//         counter : this.state.counter+1,
+//       });
+//     }
+//     return (
+//       <View style={styles.buttonContainer}>
+//         <Text>{this.state.counter}</Text>
+//         <Button title={'click me!'} onPress={clickHandler}/>
+//       </View>
+//     );
+//   };
   
-};
+// };
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <CounterButton counter={0}/>
+        {/* <CounterButton counter={0}/>
         <CounterButton />
         <CounterButton counter={2}/>
         <CounterButton counter={3}/>
-        <CounterButton counter={4}/>
+        <CounterButton counter={4}/> */}
+        <Text style={styles.text}>1</Text>
+        <Text style={styles.text}>2</Text>
+        <Text style={styles.text}>3</Text>
         <StatusBar style="auto" />
       </View>
     );  
@@ -52,11 +55,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    flexDirection: 'row', //일렬로 나열하게 배치
   },
   buttonContainer : {
     alignItems: 'center',
     width: '100%',
     borderWidth: 2,
     borderColor: 'purple',
+  },
+  text : {
+    borderWidth:1,
+    borderColor: 'red',
   }
 });
