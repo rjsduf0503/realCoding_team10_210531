27d1faf61bff0cb45440770dae5942507d76b24e
ryu <rjsduf0503@naver.com>
2021-05-31 23:06:29 +0900

    modify WeatherDetailScreen.js

--- WeatherDetailScreen.js
+++ WeatherDetailScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { Constants } from 'expo';
 
 export default class WeatherDetailScreen extends React.Component {
@@ -19,6 +19,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    marginTop: Constants.statusBarWeight,
   },
 });
